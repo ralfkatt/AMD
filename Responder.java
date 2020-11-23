b75cc01dee38113d1b7b25ffb264793b7d279f11
Maja Danielsson <maja.danielsson.5616@student.uu.se>
2020-11-23 10:00:28 +0100

    preferences i responder

--- Responder.java
+++ Responder.java
@@ -1,10 +1,15 @@
+import java.util.Date;
+
 public class Responder extends User {
     public ResponderPreferences preferences = null;
     
     public Responder(String name, String email, String address, String password) {
         super(name, email, address, password);
     }
-
+    public void setPreferences(String location, String typeOfLabour, Date date, int time, int duration, String communicationLanguage) {
+        this.preferences = new ResponderPreferences(String location, String typeOfLabour, Date date, int time, int duration, String communicationLanguage)
+    }
+    
     public void acceptRequest() {
         
     }
